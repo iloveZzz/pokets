20b85e298bca03a1c85996dda1fc8b01abd54f05
朱道明 <zhudaoming@xianbei.cc>
2017-06-09 15:52:10 +0800

    entity

--- src/main/java/com/xianbei/pocket/pojo/MysqlEntity.java
+++ src/main/java/com/xianbei/pocket/pojo/MysqlEntity.java
@@ -1,6 +1,5 @@
-package com.xianbei.pocket.domain;
+package com.xianbei.pocket.pojo;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
