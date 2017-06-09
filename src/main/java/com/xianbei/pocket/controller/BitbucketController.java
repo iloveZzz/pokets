c8bab707e72b7eb1cb136b1724a8805697b34e7b
朱道明 <zhudaoming@xianbei.cc>
2017-06-09 12:10:28 +0800

    bitbucket controller

--- src/main/java/com/xianbei/pocket/controller/BitbucketController.java
+++ src/main/java/com/xianbei/pocket/controller/BitbucketController.java
@@ -5,7 +5,9 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by zhudaoming on 2017/6/9.
@@ -14,8 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @ResponseBody
 public class BitbucketController {
     @RequestMapping("pocket_hook")
-    public String pocket_hook(String key, @RequestBody String rq, BindingResult bindingResult) {
-        System.out.println(key);
+    public String pocket_hook( @RequestBody String rq, BindingResult bindingResult) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        System.out.println(rq);
+        if (bindingResult.hasErrors()) {
+            map.put("errorCode", "000001");
+            map.put("errorMsg", bindingResult.getFieldError().getDefaultMessage());
+        }
         return "buhao";
     }
 }
